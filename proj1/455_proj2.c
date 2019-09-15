68ff2eb97bfdf304f34caa0f49e6a380859041cb
Garrett Rudisill <garrett.rudisill@gmail.com>
2019-09-15 15:03:04 -0700

    sending works, need to clean up and document more

--- proj1/455_proj2.c
+++ proj1/455_proj2.c
@@ -100,8 +100,94 @@ void send_message(){
 }
 
 void recv_message(){
+	char sender[INET6_ADDRSTRLEN];
+	int sockfd, ret, i;
+	int sockopt;
+	ssize_t numbytes;
+	struct ifreq ifopts;	/* set promiscuous mode */
+	struct ifreq if_ip;	/* get ip addr */
+	struct ifreq if_mac;
+	struct sockaddr_storage their_addr;
+	uint8_t buf[BUF_SIZ];
+	
+
+	/* Header structures */
+	struct ether_header *eh = (struct ether_header *) buf;
+	struct iphdr *iph = (struct iphdr *) (buf + sizeof(struct ether_header));
+	struct udphdr *udph = (struct udphdr *) (buf + sizeof(struct iphdr) + sizeof(struct ether_header));
+
+	memset(&if_ip, 0, sizeof(struct ifreq));
+
+	/* Open PF_PACKET socket, listening for EtherType ETHER_TYPE */
+	if ((sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETHER_TYPE))) == -1) {
+		perror("listener: socket");	
+		return -1;
+	}
+
+	//dynamically set recieving mac
+	memset(&if_mac, 0, sizeof(struct ifreq));
+	strncpy(if_mac.ifr_name, interfaceName , IFNAMSIZ-1);
+	if (ioctl(sockfd, SIOCGIFHWADDR, &if_mac)==0) {
+		printf("Sending on mac address: ");	
+  	  	for (int i = 0; i < 6; ++i)
+		{
+			printf("%02x", (unsigned char) if_mac.ifr_addr.sa_data[i]);
+			//setup source mac addy while printing
+			if(i<5)
+				printf(":");
+
+		}
+		printf("\n");
+	}
 
 
+	/* Set interface to promiscuous mode - do we need to do this every time? */
+	strncpy(ifopts.ifr_name, interfaceName, IFNAMSIZ-1);
+	ioctl(sockfd, SIOCGIFFLAGS, &ifopts);
+	ifopts.ifr_flags |= IFF_PROMISC;
+	ioctl(sockfd, SIOCSIFFLAGS, &ifopts);
+	/* Allow the socket to be reused - incase connection is closed prematurely */
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof sockopt) == -1) {
+		perror("setsockopt");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+	/* Bind to device */
+	if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, interfaceName, IFNAMSIZ-1) == -1)	{
+		perror("SO_BINDTODEVICE");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("listener: Waiting to recvfrom...\n");
+	numbytes = recvfrom(sockfd, buf, BUF_SIZ, 0, NULL, NULL);
+	while (numbytes<1)
+	{
+		//wait to recieve bytes
+	}
+	
+	printf("listener: got packet %lu bytes\n", numbytes);
+
+	/* Check the packet is for me */
+	if (eh->ether_dhost[0] == (unsigned char)if_mac.ifr_addr.sa_data[0] &&
+			eh->ether_dhost[1] == (unsigned char) if_mac.ifr_addr.sa_data[1] &&
+			eh->ether_dhost[2] == (unsigned char) if_mac.ifr_addr.sa_data[2] &&
+			eh->ether_dhost[3] == (unsigned char) if_mac.ifr_addr.sa_data[3] &&
+			eh->ether_dhost[4] ==  (unsigned char) if_mac.ifr_addr.sa_data[4] &&
+			eh->ether_dhost[5] == (unsigned char) if_mac.ifr_addr.sa_data[5] ){
+		printf("Correct destination MAC address\n");
+	} 
+
+	/* UDP payload length */
+	ret = ntohs(udph->len) - sizeof(struct udphdr);
+
+	/* Print packet */
+	printf("Data:	");
+	for (i=12; i<numbytes; i++) 
+		printf("%c", buf[i]);
+	printf("\n");
+
+	close(sockfd);
 }
 
 int main(int argc, char *argv[])
