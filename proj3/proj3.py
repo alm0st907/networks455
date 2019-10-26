f1cf8cce43a495589ba8862db097d79f735ffb35
Garrett Rudisill <garrett.rudisill@gmail.com>
2019-10-26 13:05:22 -0700

    a few more comments

--- proj3/proj3.py
+++ proj3/proj3.py
@@ -4,17 +4,11 @@ import sys
 
 #send arp to destination IP, return its HW address
 def sendARP(dest_ip):
-    #grabs our hw addr by default, only need to set arp type
     eth = Ether(type=0x806)
-    #set the destination ip, all other fields defaulte
     arph = ARP(pdst=dest_ip)
-    #assemble packets toegether
     final_packet = eth/arph
-
     if(debug):
         final_packet.show()
-
-    #send one packet and wait for answer
     resp = srp1(final_packet)
     try:
         if(resp.haslayer(ARP)):
