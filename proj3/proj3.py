e448a64b7898270481545e196ae03ef1527490a7
Garrett Rudisill <garrett.rudisill@gmail.com>
2019-10-29 14:22:38 -0700

    specified sources, need checksum

--- proj3/proj3.py
+++ proj3/proj3.py
@@ -1,20 +1,25 @@
 import socket
 from scapy.all import *
+from scapy.layers.l2 import Ether,ARP #removes Ether,IP,ARP lint errors, but is already included through scapy.all
+from scapy.layers.inet import IP
 import sys
 import netifaces
 from netaddr import *
 
 # sudo fuser -k 6653/tcp
 
+
 def displayInfo():
-    interface = conf.iface #default intesrfaces
+    interface = conf.iface  # default intesrfaces
+    print(f"Using default interface pulled: {conf.iface}")
     info = netifaces.ifaddresses(interface)
     print(f"Interface Info: {info[netifaces.AF_INET][0]}")
     return
 
+
 def determineNetmask(dest_ip):
-    
-    interface = conf.iface #default intesrfaces
+
+    interface = conf.iface  # default intesrfaces
     info = netifaces.ifaddresses(interface)
     if(debug):
         print(f"Interface Info: {info[netifaces.AF_INET][0]}")
@@ -31,14 +36,16 @@ def determineNetmask(dest_ip):
         return False
 
 # send arp to destination IP, return its HW address
+
+
 def sendARP(dest_ip):
-    
-    eth = Ether(type=0x806, dst="ff:ff:ff:ff:ff:ff")
+
+    eth = Ether(type=0x806, dst="ff:ff:ff:ff:ff:ff", src=get_if_hwaddr(conf.iface))
     arph = ARP(pdst=dest_ip)
     final_packet = eth/arph
     if(debug):
         final_packet.show()
-    resp = srp1(final_packet)
+    resp = srp1(final_packet,iface=conf.iface)
     try:
         if(resp.haslayer(ARP)):
             print("received an ARP")
@@ -51,13 +58,14 @@ def sendARP(dest_ip):
 
 
 def sendEthMsg(dest_hw_addr, msg):
-    eth = Ether(type=0x800, dst=dest_hw_addr)
+    eth = Ether(type=0x800, dst=dest_hw_addr, src = get_if_hwaddr(conf.iface))
     raw_payload = Raw(load=str(msg))
     eth.add_payload(raw_payload)
     if(debug):
         print("\nsend eth header\n")
         eth.show()
-    sendp(eth)
+    #send packet using socked on conf.iface, which is default interface (h1x1-eth0, etc)
+    sendp(eth,iface=conf.iface)
 
 
 def sendIPmsg(dest_ip, router_ip, msg):
@@ -65,23 +73,28 @@ def sendIPmsg(dest_ip, router_ip, msg):
     arp our router,
     then send 0x800 msg to router with ip of desired dest
     '''
-    
-    #compare our IP's for if we need to arp router, or IP
-    dest_hw_addr = None
+
+    # compare our IP's for if we need to arp router, or IP
+    dst_hw_addr = None
     if(determineNetmask(dest_ip)):
-        dest_hw_addr = sendARP(dest_ip)
-    #arp  our router for the HW addr
+        dst_hw_addr = sendARP(dest_ip)
+    # arp  our router for the HW addr
     else:
         dst_hw_addr = sendARP(router_ip)
-    #send 
-    eth = Ether(type=0x800, dst=dest_hw_addr)
-    iph = IP(dst=dest_ip, ttl=6,proto=253)
+    # send
+    eth = Ether(type=0x800, dst=dst_hw_addr, src = get_if_hwaddr(conf.iface))
+    # pulled protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+    # 253 is for testing and experimentation, which silenced some wireshark oddities
+    iph = IP(dst=dest_ip, ttl=6, proto=253)
+    del iph.chksum
     final_packet = eth/iph
     raw_payload = Raw(load=str(msg))
     final_packet.add_payload(raw_payload)
     if(debug):
         final_packet.show()
-    sendp(final_packet)
+    final_packet.show2()
+    #send packet using socked on conf.iface, which is default interface (h1x1-eth0, etc)
+    sendp(final_packet, iface = conf.iface)
 
 
 if __name__ == "__main__":
@@ -89,11 +102,13 @@ if __name__ == "__main__":
     debug = 0
     # send using IP
     if(sys.argv[1] == "ip"):
+        displayInfo()
         sendIPmsg(sys.argv[2], sys.argv[3], sys.argv[4])
         exit(0)
 
     # send ethernet message
     if(len(sys.argv) > 2 and sys.argv[1] == "send"):
+        displayInfo()
         dest_hw = sendARP(str(sys.argv[2]))
         sendEthMsg(dest_hw, str(sys.argv[3]))
         exit(0)
@@ -106,8 +121,8 @@ if __name__ == "__main__":
 
             # use sniff to recieve packets on our default interface
             print("Listening for message")
-            msg_pkt = sniff(count=1, filter="ip")
-            #show because sometimes the packet is getting clobbered
+            msg_pkt = sniff(count=1, filter="ip", iface=conf.iface)
+            # show because sometimes the packet is getting clobbered
             # msg_pkt[0].show()
 
             # ensure the package has our raw payload, and is for us
