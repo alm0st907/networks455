e320ebbcee95fa75d5d4ac2691bfa504bf8017b7
Garrett Rudisill <garrett.rudisill@gmail.com>
2019-12-01 15:20:02 -0800

    working on resend of dropped, ack sequencing

--- final/client.py
+++ final/client.py
@@ -1,52 +1,57 @@
-# from scapy.all import *
-# import socket
-# import sys
-
-# global debug
-
-# if __name__ == "__main__":
-#     debug = 0
-#     file = open('tux.txt','r')
-#     lines = file.readlines()
-#     if debug:
-#         for line in lines:
-#             print(line,end="") #establish no newline on end
-#     if(len(sys.argv) < 3 and not debug):
-#         print("ARGS <ip> <filename>")
-    
 import socket
 from scapy.all import *
 
-def waitForAck():
+#sudo mn --mac --switch ovsk --controller=remote,ip=0.0.0.0,port=6633
+
+def waitForAck(ack_sequence):
     print("waiting for ack")
     while(True):
         newsock = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
-        newsock.bind((UDP_IP, 5007))
+        newsock.bind((RECV_UDP_IP, 5007))
         newsock.settimeout(5)
         try: 
             data, addr = newsock.recvfrom(1024) # buffer size is 1024 bytes
-            if data.decode('UTF-8') == '0123456789' :
+            print(addr)
+            data = data.decode('UTF-8')
+            if data == ack_sequence :
                 print('we acked')
                 newsock.close()
                 break
             else:
-                print(f'expected: {buffer}\nrecieved: {data.decode("UTF-8")}')
+                print(f'expected: {ack_sequence}\nrecieved: {data}')
+                missing_packets = ''
+                for c in ack_sequence:
+                    if c not in data:
+                        missing_packets += c
+                
+                print(f'Need to resend packets {missing_packets}')
 
         except (socket.timeout):
             print('failed to ack')
             newsock.close()
             exit(1)
 
+def sendMissingPackets():
+    pass
+
 if __name__ == "__main__":
     
     if (len(sys.argv)<3):
         exit(1)
-    global UDP_IP
-    UDP_IP = sys.argv[1]
+    global DEST_UDP_IP
+    global RECV_UDP_IP
+    DEST_UDP_IP = sys.argv[1]
     filename = sys.argv[2]
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("10.0.0.1", 80))
+    print(s.getsockname()[0])
+    RECV_UDP_IP = s.getsockname()[0] # get local ip
+    s.close()
+
     print("Opening file:",filename)
-    print("UDP target IP:", UDP_IP)
+    print("UDP target IP:", DEST_UDP_IP)
     # print("UDP target port:", UDP_PORT)
     UDP_PORT = 5005
 
@@ -59,18 +64,22 @@ if __name__ == "__main__":
     sentPackets = 0
     i = 0
     packetBuffer = dict()
+    windowAcqSequence = '' #packets ack sequence we are sending, eg 0-(number of packets -1) with a max of 10
 
+    #need to send how many packets expected in a given window if less than 10
     for line in lines:
         if i is 10:
             # print(packetBuffer) # so we have a dict of packets to determine which ones
-            waitForAck()
+            waitForAck(windowAcqSequence)
             i = 0
+            windowAcqSequence
             packetBuffer = dict()
-        sock.sendto(bytes(str(i)+line,encoding='UTF-8'), (UDP_IP, UDP_PORT))
+        sock.sendto(bytes(str(i)+line,encoding='UTF-8'), (DEST_UDP_IP, UDP_PORT))
         packetBuffer[i]=line
+        windowAcqSequence += str(i)
         i+=1
 
-    sock.sendto(bytes('term',encoding='UTF-8'), (UDP_IP, UDP_PORT))
+    # sock.sendto(bytes('term',encoding='UTF-8'), (DEST_UDP_IP, UDP_PORT))
     print("completed send")
 
     # waitForAck()
--- final/serv.py
+++ final/serv.py
@@ -2,49 +2,82 @@ from scapy.all import *
 import socket
 global debug
 
+#sudo mn --mac --switch ovsk --controller=remote,ip=0.0.0.0,port=6633
 
 def sendAck(buffer):
     newsock = socket.socket(socket.AF_INET,  # Internet
         socket.SOCK_DGRAM)  # UDP
-    newsock.sendto(bytes(buffer, encoding='UTF-8'), (UDP_IP, 5007))
+    buffer = ''.join(sorted(set(buffer)))
+    newsock.sendto(bytes(buffer, encoding='UTF-8'), ("10.0.0.1", 5007))
     newsock.close()
+    missing_packets = ''
     print(f'sent ack buf:{buffer}')
+    for c in '0123456789':
+        if c not in buffer:
+            missing_packets += c
+    print(f'missing packets {missing_packets}')
+
+#function to encapsulate recieving process
+def recvPackets():
+    pass
+
+#need to know how many packets are expected
+def getMissingPackets(missingPackets, packetBuffer):
+    pass
 
 
 if __name__ == "__main__":
 
     print('Project 4 client')
 
+    # establish local IP
     global UDP_IP
-    UDP_IP = '192.168.0.6'
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("10.0.0.1", 80))
+    # print(s.getsockname()[0])
+    UDP_IP = s.getsockname()[0] # get local ip
+    s.close()
+
     UDP_PORT = 5005
 
+    #setup for sending
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
     sock.bind((UDP_IP, UDP_PORT))
-    sock.settimeout(4)
+    sock.settimeout(2)
     file = open("output.txt", "a+")
     i = 0
-    recvBuffer = ''
+    recvBuffer = '' #the ack numbers from packets into a string
+    recvPacketsBuffer = {} #packets recieved in a dictionary, key is the ack #
     while True:
         try:
             if i is 10:
                 sendAck(recvBuffer)
                 i = 0
                 recvBuffer = ''
+                for x in sorted(recvPacketsBuffer.keys()):
+                    file.write(recvPacketsBuffer[x])
+                recvPacketsBuffer = {}
+
             data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
             # print(data.decode('UTF-8'), end='')
             if (data.decode('UTF-8') == 'term'):
                 print('message received, terminate connection')
                 sock.close()
                 exit(0)
-
-            file.write(data.decode('UTF-8')[1:])
+            recvPacketsBuffer[data.decode('UTF-8')[0]] = data.decode('UTF-8')[1:]
             recvBuffer += data.decode('UTF-8')[0]
+
             i+=1
         except (socket.timeout):
             print("Socket timeout, closing")
-            sendAck('term')
+            print("flushing buffer")
+
+            #flushing the file in order of keys
+            for x in sorted(recvPacketsBuffer.keys()):
+                file.write(recvPacketsBuffer[x])
+
+            sendAck(recvBuffer)
             break
     
 
