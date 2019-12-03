12b4fb8cc43dbe9f0619e0aa815e3e7a0314357a
Garrett Rudisill <garrett.rudisill@gmail.com>
2019-12-03 14:20:35 -0800

    commenting some stuff for partial submission

--- final/client.py
+++ final/client.py
@@ -1,20 +1,40 @@
+'''
+garrett rudisill
+sending side of the project
+'''
 import socket
 from scapy.all import *
 
+# python way to cross platform get IP
 def getIP():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("10.0.0.1", 80))
     print(s.getsockname()[0])
-    IP = s.getsockname()[0] # get local ip
+    IP = s.getsockname()[0]  # get local ip
     s.close()
     return IP
 
-def recvACK(sock):
+
+# doing go back n, with a given buffer window, N start point
+def sendPacket(buffer, n, sendIP, port,sock):
+    while n < len(buffer):
+        data = buffer[n]
+        # print(data,end='')
+        sock.sendto(bytes(str(n)+data, encoding='UTF-8'), (sendIP, port))
+        n += 1
+
+#shortcut to just send a termination signal
+def sendTerm(sendIP,port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(bytes('term', encoding='UTF-8'), (sendIP, port))
+    sock.close()
+
+
+# either we recieve ack, or n position to start at
+def recvACK(recvIP, port,sock):
     # print(recvIP)
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # sock.bind((recvIP, port))
-    # sock.settimeout(5)
     data = None
+    sock.settimeout(3) #no response for 3 seconds, connection is borked and we timeout
     while (data is None) or (data is '') or (data is '\x00'):
         try:
             data, addr = sock.recvfrom(1024)
@@ -25,50 +45,73 @@ def recvACK(sock):
 
             if data == '' or data == '\x00': pass
             else:
-                return int(data)
+                sock.settimeout(0) #remove the timeout 
+                return int(data) #returning where we need to restart at
         except(socket.timeout):
             print('socket timed out, exiting')
+            sendTerm(sendIP,port)
             sock.close()
             exit(1)
 
-def sendACK(sendIP,port,ack):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # ack = f'{ack}'.encode("UTF-8")
-    sock.sendto(bytes(str(ack),encoding='UTF-8'), (sendIP, port))
-    sock.close()
+
+# take our file and split into 10 packet windows
+# returns a list of windows with up to 10 packets
+
+def makeWindows(fileLines):
+    windows = []
+    buffer = []
+    maxLines = 10
+    for line in lines:
+        if len(buffer) == maxLines:
+            windows.append(buffer)
+            buffer = []
+
+        buffer.append(line)
+        #if our window has less than 10 packets, making sure it gets added into the buffer of windows
+        if len(buffer) < 10 and line == lines[len(lines)-1]:
+            windows.append(buffer)
+    return windows
 
 
 if __name__ == "__main__":
-    recvIP = getIP()
+    if (len(sys.argv) < 3): #if we dont have ip/file args we exit
+        exit(1)
+    window = 10
     port = 5005
-    output = open('output.txt', 'w+')
-    print("started client")
+    sendIP = sys.argv[1]
+    filename = sys.argv[2]
+    recvIP = getIP()  # establish our IP
+
+    #parse file into list of lines
+    file = open(filename, 'r')
+    lines = file.readlines()
+
+    #create and bind our UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((recvIP, port))
-    nextPacket = 0
-    while True:
+    # sock.settimeout(5)
 
-        data, addr = sock.recvfrom(1024)
-        data = data.decode('UTF-8')
-        msg = data[1:]
-        # print(data)
-        if data =='term' or data[1:] == 'term':
-            print('finished')
-            sock.close()
-            exit(0)
-        packetNum = int(data[0])
-        print(f'expected {nextPacket} recieved packet {packetNum}')
-        # output.write(msg)
-        if nextPacket == packetNum:
-            output.write(msg)
-            nextPacket+=1     
-        else:
-            print('Dropped packet')
-            print(nextPacket)
-            sendACK(addr[0],port,nextPacket)
-            # time.sleep(1)
+    #make our windows from the lines
+    buffer = makeWindows(lines)
+    termWindow = ['term'] #redundant window with term to assist if term message gets dropped
+    buffer.append(termWindow)
 
-            #set number and be ready to loopback
-        if nextPacket == 10:
-            nextPacket = 0
-            sendACK(addr[0],port,'ACK')
+    sock.settimeout(3) # timeout of 3 seconds so we eventually term the client
+    #iterate through windows
+    for window in buffer:
+        n = 0  # start at 0
+        # we only advance to next window when current window has been fully acked
+        while n!= window:
+            #send our window, initially starting at n position of 0 (initial send)
+            sendPacket(window, n, sendIP, port,sock)
+            # print('Sent Window')
+            
+            # n is either the ACK of 10 (we got the full window)
+            # otherwise n is what the expected packet was and not received
+            # this will loop to resend window from that index on (packet SN's and buffer are both 0 indexed)
+            n = recvACK(recvIP, port,sock) 
+            print(f'Acked {n}') #debugging what the ack was
+
+    #send a termination since we are done sending windows
+    sendTerm(sendIP,port)
+    print('sent term')
---
+++ final/server.py
@@ -0,0 +1,70 @@
+'''
+garrett rudisill
+receive side of project
+'''
+import socket
+from scapy.all import *
+
+def getIP():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("10.0.0.1", 80))
+    print(s.getsockname()[0])
+    IP = s.getsockname()[0] # get local ip
+    s.close()
+    return IP
+
+#may need to pass in premade socket for this to work right rather than creating a socket, sending the ack then closing
+def sendACK(sendIP,port,ack):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(bytes(str(ack),encoding='UTF-8'), (sendIP, port))
+    sock.close()
+
+
+if __name__ == "__main__":
+    recvIP = getIP()
+    port = 5005
+
+    #open our output file to write, will create new file if already exists
+    output = open('output.txt', 'w+')
+    print("started client")
+
+    #create and bind the socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((recvIP, port))
+
+    #expected SN of packet to start
+    nextPacket = 0
+    while True:
+
+        data, addr = sock.recvfrom(1024)
+        data = data.decode('UTF-8') #decode bytes to regular string
+        msg = data[1:] #strip off SN to have the line to write
+        # print(data)
+        
+        # server termination clause, whether from the explict term or the window term
+        if data =='term' or data[1:] == 'term':
+            print('finished')
+            sock.close()
+            exit(0)
+        
+        #if its not term, we strip the SN from packet to check if its the expected packet
+        packetNum = int(data[0])
+        print(f'expected {nextPacket} recieved packet {packetNum}')
+        # output.write(msg)
+
+        #if the packet is our expected packet, write msg to file, advance the next packet expected
+        if nextPacket == packetNum:
+            output.write(msg)
+            nextPacket+=1     
+        #if the packet is not expeceted, we nack the packet we were expecting so the client can resend from that point on
+        else:
+            print('Dropped packet')
+            print(nextPacket)
+            sendACK(addr[0],port,nextPacket)
+            # time.sleep(1)
+
+        # if our next packet hits 10, we have recieved all packets (sn is 0-9 for 10 packets total)
+        # reset to next packet of 0, send ACK to client to advance the window
+        if nextPacket == 10:
+            nextPacket = 0
+            sendACK(addr[0],port,'ACK')
